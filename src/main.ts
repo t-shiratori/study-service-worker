a7bac5233bbcbecae243923a5df0db85e0c06354
Takashi Shiratori <tks.srtr89@gmail.com>
2022-11-19 13:32:41 +0900

    add statechange

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,31 @@ export const registerServiceWorker = async () => {
 			const registration = await navigator.serviceWorker.register('/service-worker.js', {
 				scope: '/',
 			})
+
+			let serviceWorker
+
 			_clg({ text: 'main.js registration', data: registration })
+
 			if (registration.installing) {
+				serviceWorker = registration.installing
 				_clg({ text: 'main.js registration.installing', data: registration.installing })
 				_clg({ text: 'main.js registration.installing.state', data: registration.installing?.state })
 			} else if (registration.active) {
+				serviceWorker = registration.active
 				_clg({ text: 'main.js registration.active', data: registration.active })
 				_clg({ text: 'main.js registration.active.state', data: registration.active?.state })
 			} else if (registration.waiting) {
+				serviceWorker = registration.waiting
 				_clg({ text: 'main.js registration.waiting', data: registration.waiting })
 				_clg({ text: 'main.js registration.waiting.state', data: registration.waiting?.state })
+			} else {
+				_clg({ text: 'main.js registration other' })
+			}
+
+			if (serviceWorker) {
+				serviceWorker.addEventListener('statechange', function (e: Event) {
+					_clg({ text: 'main.js statechange e.target.state', data: e.target?.state })
+				})
 			}
 		} catch (error) {
 			_clg({ text: `main.js Registration failed with ${error}` })
